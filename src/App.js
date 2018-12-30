b2561d91a6d83f65db0a33d1deb240f60152287e
Hannah Stahl <hls62@cornell.edu>
2018-12-30 17:39:50 -0600

    Created React app and added privacy policy

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            <b>Privacy Policy for www.niccatterall.com</b>
+            <br />
+            <br />
+            This website does not collect any user information. 
+            The website does not provide any capabilities for login, purchase, or any other user data entry. 
+            The website also does not collect any analytics on its users. 
+            Photos from Nic Catterall's Instagram feed appear on the website, as he provided permission to the web developer to display them. 
+            However, no visitor of www.niccatterall.com is able to like, comment on, edit, or in any way affect these Instagram posts from the website. 
+            Taking any such action would require navigating to and logging into the Instagram app or website.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
